c287bc861f03e227c64902b9653713a8f46001d3
Robert Pyke <robertpyke@fb.com>
2020-04-13 16:29:06 -0700

    Update repo structure so the setup.py and example config are exported to github (to be compatible with pip install)
    Summary:
    Moves src files into a subdirectory fbauthenticator.
    
    Moves setup.py into the github repo, to make the repo compatible with pip install. See https://pip.pypa.io/en/stable/reference/pip_install/#git for more info.
    
    Renamed jupyterhub_config.py to example_jupyterhub_config.py and moved it into the git repo.
    
    Differential Revision: D20998455
    
    fbshipit-source-id: fba72a9c4d892421e8e2fcd38f90fd9e5b36fb46

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name="jupyterhub-fbauthenticator",
+    version="0.1",
+    description="FB Authenticator for Jupyterhub",
+    author="Facebook",
+    author_email="researchtool-help@fb.com",
+    url="https://www.facebook.com/",
+    packages=["fbauthenticator"],
+)
