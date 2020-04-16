511b338eef5835cd2c020ddacc515fd9a1078fa5
Robert Pyke <robertpyke@fb.com>
2020-04-16 14:30:12 -0700

    Request business_management permission from user as part of login flow.
    Summary: Request business_management permission from user as part of login flow.
    
    Differential Revision: D21071569
    
    fbshipit-source-id: 07d480fc3af458f38f0df742e11292785bdc5627

--- fbauthenticator/business_authenticator.py
+++ fbauthenticator/business_authenticator.py
@@ -21,6 +21,8 @@ from .authenticator import FBAuthenticator
 
 class FBBusinessAuthenticator(FBAuthenticator):
 
+    scope = ["business_management", "email"]
+
     BUSINESS_ID = os.environ.get("BUSINESS_ID")
     PAGE_THRESHOLD = 100
 
